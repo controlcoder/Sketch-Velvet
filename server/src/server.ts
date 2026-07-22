6735e1ae8c6cfad0e8f1ed72d29b30c97f375834
controlcoder <ritikaryan2210@gmail.com>
2026-07-22 10:09:38 +0530

    update prisma setup to v6

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,24 @@
 import dotenv from "dotenv";
-
 dotenv.config();
 
 import app from "./app";
+import { prisma } from "./config/prisma";
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+
+async function start() {
+  try {
+    await prisma.$connect();
+
+    console.log("Database Connected");
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+start();
